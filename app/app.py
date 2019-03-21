2b148970248e794f1f638bdc82064e315e8065de
Jonathan Langens <flowofcontrol@gmail.com>
2019-03-21 09:50:30 +0100

    write to file instead of to std out

--- app/app.py
+++ app/app.py
@@ -1,19 +1,20 @@
 import sys
+import uuid
 from kafka import KafkaConsumer
 from json import dumps
 from json import loads
 
-print("creating")
 consumer = KafkaConsumer(
   "checked-application-logs",
   bootstrap_servers=['kafka:9092'],
   auto_offset_reset='earliest',
   enable_auto_commit=True,
-  group_id="12345678",
+  group_id=str(uuid.uudi4()),
   value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-print("reading")
+print("starting to write...")
 sys.stdout.flush()
+f= open("/logs/logs","w+")
 
 for message in consumer:
   message = message.value
@@ -22,5 +23,7 @@ for message in consumer:
   time_stamp = float(message["timestamp"])
   checked_time_stamp = float(message["checkedTimestamp"])
   time_spent = abs(time_stamp - checked_time_stamp)
-  print(str(message_id) + ";" + str(time_stamp) + ";" + str(checked_time_stamp) + ";" + str(time_spent))
-  sys.stdout.flush()
+  log_message = str(message_id) + ";" + str(time_stamp) + ";" + str(checked_time_stamp) + ";" + str(time_spent)
+  f,write(log_message)
+
+f.close()
